1ce04d7feb1bd59d19514726111dd953c85c9f92
federicotrani <federicotrani@gmail.com>
2017-08-07 18:19:20 -0300

    Se incluye menu de contexto

--- app/src/main/java/com/example/ftrani/menuopcionites/MainActivity.java
+++ app/src/main/java/com/example/ftrani/menuopcionites/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.ftrani.menuopcionites;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,33 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
-        //defino array con nombres a mostra en listview
-        List<String> nombres = new ArrayList<String>();
-        nombres.add("Juan");
-        nombres.add("Pedro");
-        nombres.add("Jose");
-        nombres.add("Juan");
-        nombres.add("Pedro");
-        nombres.add("Jose");
-        nombres.add("Juan");
-        nombres.add("Pedro");
-        nombres.add("Jose");
-        nombres.add("Juan");
-        nombres.add("Pedro");
-        nombres.add("Jose");
-        //defino adaptador
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,nombres);
-        //Asigno adaptador al listview
-        listView.setAdapter(adapter);
+        ImageView imgAvatar = (ImageView) findViewById(R.id.imgAvatar);
+        //habilitamos soporte para menu contextual
+        registerForContextMenu(imgAvatar);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
     }
 
@@ -61,7 +46,27 @@ public class MainActivity extends AppCompatActivity {
             case R.id.mnuCerrar:
                 finish();
                 return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_contexto, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mnuEditar:
+                Toast.makeText(MainActivity.this,"Selecciono Editar!!",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.mnuBorrar:
+                Toast.makeText(MainActivity.this,"Seleccione Borrar!!",Toast.LENGTH_SHORT).show();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
